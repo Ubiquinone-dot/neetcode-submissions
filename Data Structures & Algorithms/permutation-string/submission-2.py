4c43d2f7569b9e9385e60196d72f5eeccc85a6a3
Jasper Butcher <66851659+Ubiquinone-dot@users.noreply.github.com>
2026-06-05 21:38:29 -0700

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,51 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        l = 0
+
+        # Create a count dict of s1
+        countS1 = {}
+        for c in s1:
+            countS1[c] = 1 + countS1.get(c, 0)
+
+        # Slide window over s2, checking equivalence to s1
+        window = {}
+        l = 0
+        for r in range(len(s2)):
+
+            # Increment window
+            window[s2[r]] = 1 + window.get(s2[r], 0)
+            
+            def is_match(window):
+                return all([
+                    window.get(c, 0) == countS1.get(c, 0)
+                    for c in set(window.keys()) | set(countS1.keys())
+                ])
+
+
+            def is_superset(window):
+                return all([
+                    window.get(c, 0) >= countS1[c]
+                    for c in countS1
+                ])
+
+            # Check if the window contains a permutation of S1 as a substring
+        
+            if is_match(window):
+                print(True, window)
+                return True
+            
+            print(r, window)
+
+            while is_superset(window) and sum(list(window.values())):
+                print("l", window)
+                # Slide window by one.
+                window[s2[l]] -= 1
+                l += 1
+                
+                # Check again if is match
+                if is_match(window):
+                    print(True, 2, window)
+                    return True
+
+
+        return False
