faddb766daa93db8ebd49afbdc7b7abbc639d452
Jasper Butcher <66851659+Ubiquinone-dot@users.noreply.github.com>
2026-06-05 22:19:43 -0700

    Add: binary-search - submission-8

---
+++ Data Structures & Algorithms/binary-search/submission-8.py
@@ -0,0 +1,37 @@
+class Solution:
+    
+    # def binary_search(self, l, r, nums, target):        
+    #     if l > r:
+    #         return -1
+
+    #     # Find midpoint; middle of r-l window offset by l
+    #     mid = l + ((r - l ) // 2) # floored.
+
+    #     if nums[mid] == target:
+    #         return mid
+        
+    #     if nums[mid] < target:
+    #         # must be on upper half
+    #         return self.binary_search(mid+1, r, nums, target)
+    #     else:
+    #         return self.binary_search(l, mid - 1, nums, target)
+
+    def search(self, nums: List[int], target: int) -> int:
+        # return self.binary_search(0, len(nums) - 1, nums, target)
+
+        l, r = 0, len(nums)-1
+
+        while l <= r:
+
+            mid = l + ((r - l) // 2)
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid+1
+            else:
+                r = mid-1
+            
+        return -1
+
+
