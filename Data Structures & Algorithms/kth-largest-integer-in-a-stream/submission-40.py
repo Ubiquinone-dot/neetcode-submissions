3c937ce368eacb8a79a48a89d84080d852b77d4c
Jasper Butcher <66851659+Ubiquinone-dot@users.noreply.github.com>
2026-06-07 23:19:37 -0700

    Add: kth-largest-integer-in-a-stream - submission-40

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-40.py
@@ -0,0 +1,31 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.minval = -float("infinity")
+        self.stream = sorted(nums, reverse=True)[:k]
+
+    def add(self, val: int) -> int:
+        # Binary alg to find insertion point
+        if val < self.minval:
+            return self.stream[0]
+        
+        self.stream.append(val)
+        self.stream = sorted(self.stream, reverse=True)
+        print(self.stream)
+        return self.stream[self.k-1]
+
+        # n = len(self.stream)
+        # l, r = 0, n-1
+        # while (r - l) > 0:
+        #     m = (r - l + 1) // 2
+        #     v = self.stream[m]
+        #     if val > v:
+        #         l = m
+        #     elif val <= v:
+        #         r = m
+        #     print(l, r, m, self.stream, self.stream[m], val)
+        #     break
+        # return self.stream[self.k]
+
+
