42a3fdf0c6bcf14cc61c878a5b40bf3431dced2e
Jasper Butcher <66851659+Ubiquinone-dot@users.noreply.github.com>
2026-06-08 18:07:46 -0700

    Add: climbing-stairs - submission-64

---
+++ Data Structures & Algorithms/climbing-stairs/submission-64.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        prev2 = 1
+        prev = 1
+        for i in range(n-1):
+            tmp = prev
+            prev = prev + prev2
+            prev2 = tmp
+        return prev
