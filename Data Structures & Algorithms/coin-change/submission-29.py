20ea05b7d89e2b6a29f813608a6911d57edde129
Jasper Butcher <66851659+Ubiquinone-dot@users.noreply.github.com>
2026-07-08 21:23:04 -0700

    Add: coin-change - submission-29

---
+++ Data Structures & Algorithms/coin-change/submission-29.py
@@ -0,0 +1,35 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:   
+        dp = [0] + [float('inf')] * amount
+
+        for i in range(1, amount+1):
+            for c in coins:
+                rem = i - c
+                if rem >= 0:
+                    dp[i] = min(dp[i], dp[rem] + 1)
+
+        return dp[-1] if dp[-1] != float('inf') else -1
+        # dp = {c: 1 for c in coins}
+        # coins = sorted(coins, reverse=True)
+        # if amount==0:
+        #     return 0
+
+        # def dfs(left):
+        #     if left in dp:
+        #         return dp[left]
+        #     found=False
+        #     for c in coins:
+        #         rem = left - c
+        #         if rem > 0:
+        #             dfs(rem)
+        #             if dp.get(rem, -1) > 0:
+        #                 dp[left] = min(dp[rem] + 1, dp.get(left, float('inf')))
+        #                 found=True
+
+        #     if not found:
+        #         dp[left] = -1
+        #     return
+
+        # dfs(amount)
+        # print(dp)
+        # return dp[amount]
