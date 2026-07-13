fd114d948f26295771cf978e0db26f626b582ead
Jasper Butcher <66851659+Ubiquinone-dot@users.noreply.github.com>
2026-07-12 20:39:58 -0700

    Add: generate-parentheses - submission-5

---
+++ Data Structures & Algorithms/generate-parentheses/submission-5.py
@@ -0,0 +1,59 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+
+        res = []
+        stack = []
+
+        def generate(opened, closed):
+            if opened == closed == n:
+                res.append("".join(stack))
+                return
+
+            # left and right calls
+            if opened + closed >= n*2:
+                return
+
+            # open
+            if opened <= n:
+                stack.append("(") # maintain a stack, but passing variables associated with that stack around.
+                generate(opened+1, closed)
+                stack.pop()
+            
+            # closing
+            if closed < opened:
+                stack.append(")")
+                generate(opened, closed+1)
+                stack.pop()
+            return
+
+        generate(0,0)
+        
+        return res
+        
+        # if n == 1:
+        #     return ['()']
+        # elif n == 2:
+        #     return ['(())', '()()']
+        # # elif n == 3:
+        # #     return ['((()))', '(()())', '(())()', '()(())', '()()()']
+
+        # def dfs(n):
+        #     if n == 1:
+        #         return ['()']
+            
+        #     sub = dfs(n-1) 
+        #     res = []
+        #     n_bracket = len(sub[0]) // 2 + 1
+        #     for i in range(len(sub)):
+        #         for j in range(n_bracket):
+        #             brackets = sub[i] # str 
+        #             brackets = brackets[:j] + "()" + brackets[j:]
+        #             if brackets not in res:
+        #                 res.append(
+        #                     brackets
+        #                 )
+        #             # print(i, j, sub[i], res)
+
+        #     return res
+
+        # return list(reversed(dfs(n)))
